188ba8f690484bdb4f0d1b586a93897325b73e37
Bishal Thapaliya <vishal.thapaliya@gmail.com>
2024-12-06 18:18:07 +0100

    add react-icons (#12)

--- userList-app-using-useEffect/src/components/UsersCard.jsx
+++ userList-app-using-useEffect/src/components/UsersCard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Loading from './Loading';
+import { FaGithub } from "react-icons/fa6";
+import { FcApproval } from "react-icons/fc";
 
 const UsersCard = () => {
     const [users, setUsers] = useState([]);
@@ -28,7 +30,7 @@ const UsersCard = () => {
 
     return (
         <>
-            <h2 className='title'>Github Users</h2>
+            <h2 className='text-3xl flex justify-center items-center gap-2 m-4'><FaGithub /> Github Users</h2>
 
             <div className="max-w-full w-screen h-screen flex justify-between items-center flex-wrap gap-4">
                 {
@@ -42,12 +44,8 @@ const UsersCard = () => {
                                     </div>
                                 </div>
                                 <div className="relative flex justify-between items-center flex-row px-6 z-50 -mt-10">
-                                    <button className="p-4 bg-red-600 rounded-full hover:bg-red-500 focus:bg-red-700 transition ease-in duration-200 focus:outline-none">
-                                        <svg viewBox="0 0 20 20" enableBackground="new 0 0 20 20" className="w-6 h-6">
-                                        <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
-                                                                C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
-                                                                C15.952,9,16,9.447,16,10z" />
-                                        </svg>
+                                    <button className="p-4 verified-icon">
+                                        <FcApproval />
                                     </button>
                                 </div>
                                 <div className="pt-6 pb-8 text-gray-600 text-center">
